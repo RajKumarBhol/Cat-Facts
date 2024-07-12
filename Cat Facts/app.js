a1f78385051ec35ec54f9f7fac82b26d6e67cca4
Raj Kumar Bhol <137034819+RajKumarBhol@users.noreply.github.com>
2024-07-12 21:04:17 +0530

    Add files via upload

---
+++ Cat Facts/app.js
@@ -0,0 +1,27 @@
+let url="https://catfact.ninja/fact";
+
+let btn=document.querySelector("button");
+
+
+async function getFacts(){
+    try{
+        let res=await axios.get(url);
+        return res.data.fact;
+    }
+    catch(e){
+        console.log("error: ",e);
+        return "NO FACT FOUND";
+    }
+}
+
+
+
+
+btn.addEventListener("click",async()=>{
+    let facts=await getFacts();
+    console.log(facts);
+    let p=document.querySelector("#result");
+    p.innerHTML=facts;
+    });
+
+
